e1312a5c24bdf42da9a08fd055aa104f428f4c3c
katsuxD <khanhb5192@gmail.com>
2022-11-11 23:19:51 +0700

    Music app learning from https://www.youtube.com/watch?v=12ktVNkKw5w&t=93s&ab_channel=MaxonFlutter, meet some maven sdk bugs when try to run app on mobile device

---
+++ Day 21 to 30/Day23/music_app_leaning/lib/screens/home_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+            Colors.deepPurple.shade800.withOpacity(0.8),
+            Colors.deepPurple.shade200.withOpacity(0.8),
+          ])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: const _CustomAppbar(),
+        bottomNavigationBar: _CustomNavbar(),
+        body: Container(),
+      ),
+    );
+  }
+}
+
+class _CustomNavbar extends StatelessWidget {
+  const _CustomNavbar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.deepPurple.shade800,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline), label: "Favorites"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.play_circle_fill_outlined), label: "Play"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.people_outline), label: "Profile"),
+        ]);
+  }
+}
+
+class _CustomAppbar extends StatelessWidget with PreferredSizeWidget {
+  const _CustomAppbar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      leading: const Icon(Icons.grid_view_rounded),
+      actions: [
+        Container(
+          margin: const EdgeInsets.only(right: 20),
+          child: const CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://avatars.githubusercontent.com/u/52829521?s=400&u=1ba0bd809eaf72e6bdfcfe8b96a2942c8d3f9b5d&v=4"),
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56.0);
+}
