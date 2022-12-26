45d97ebdb7a4884eb794f2dd4b9707485b95ccf2
katsuxD <khanhb5192@gmail.com>
2022-12-26 23:27:43 +0700

    day26 - learning to make chat app UI WhatsApp responsive link: https://www.youtube.com/watch?v=g1hmeP8WZHU&ab_channel=RivaanRanawat

---
+++ Day 21 to 30/Day26/chat_app_ui_whatsapp_clone/lib/screens/mobile_screen_layout.dart
@@ -0,0 +1,49 @@
+import 'package:chat_app_ui_whatsapp_clone/colors.dart';
+import 'package:chat_app_ui_whatsapp_clone/widgets/contacts_list.dart';
+import 'package:flutter/material.dart';
+
+class MobileScreenLayout extends StatelessWidget {
+  const MobileScreenLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: appBarColor,
+          title: const Text(
+            "Whats app",
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: false,
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.search, color: Colors.grey)),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert, color: Colors.grey)),
+          ],
+          bottom: const TabBar(
+              indicatorColor: tabColor,
+              indicatorWeight: 4,
+              labelColor: tabColor,
+              labelStyle: TextStyle(fontWeight: FontWeight.bold),
+              unselectedLabelColor: Colors.grey,
+              tabs: [
+                Tab(text: 'CHATS'),
+                Tab(text: 'STATUS'),
+                Tab(text: 'CALLS')
+              ]),
+        ),
+        body: const ContactsList(),
+      ),
+    );
+  }
+}
