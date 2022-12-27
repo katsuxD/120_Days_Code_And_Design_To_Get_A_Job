06d09fff1ea1c374cabf7bdb54b54703c44509fa
katsuxD <khanhb5192@gmail.com>
2022-12-27 23:57:55 +0700

    update web view

--- Day 21 to 30/Day26/chat_app_ui_whatsapp_clone/lib/screens/web_screen_layout.dart
+++ Day 21 to 30/Day26/chat_app_ui_whatsapp_clone/lib/screens/web_screen_layout.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app_ui_whatsapp_clone/widgets/contacts_list.dart';
+import 'package:chat_app_ui_whatsapp_clone/widgets/web_profile_bar.dart';
 import 'package:flutter/material.dart';
 
 class WebScreenLayout extends StatelessWidget {
@@ -5,8 +7,33 @@ class WebScreenLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("This is web")),
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  WebProfileBar(),
+                  // Web search
+                  ContactsList(),
+                ],
+              ),
+            ),
+          ),
+          //Web Screen
+          Container(
+            width: MediaQuery.of(context).size.width * 0.75,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/backgroundImage.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
