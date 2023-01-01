e4b9203ea57c4d8958e373b05d9af6542e58b7bd
katsuxD <khanhb5192@gmail.com>
2023-01-01 22:54:04 +0700

    Prepare for the project chat app learn from https://www.youtube.com/watch?v=yqwfP2vXWJQ&t=501s&ab_channel=RivaanRanawat

---
+++ Day 21 to 30/Day27 to Day30/chat_app_whatsapp_clone/lib/widgets/web_profile_bar.dart
@@ -0,0 +1,44 @@
+import 'package:chat_app_whatsapp_clone/colors.dart';
+import 'package:flutter/material.dart';
+
+class WebProfileBar extends StatelessWidget {
+  const WebProfileBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.077,
+      width: MediaQuery.of(context).size.width * 0.25,
+      padding: const EdgeInsets.all(10),
+      decoration: const BoxDecoration(
+          border: Border(
+            right: BorderSide(
+              color: dividerColor,
+            ),
+          ),
+          color: webAppBarColor),
+      child: Row(children: [
+        const CircleAvatar(
+          backgroundImage: NetworkImage(
+              "https://pbs.twimg.com/profile_images/1419974913260232732/Cy_CUavB.jpg"),
+          radius: 20,
+        ),
+        const Spacer(),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.comment,
+            color: Colors.grey,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.more_vert,
+            color: Colors.grey,
+          ),
+        ),
+      ]),
+    );
+  }
+}
